c378ace6e39c5585c1654ed8913a5bab551f9fbe
Thaniyarasi <thaniyarasik@gmail.com>
2026-02-02 13:24:13 +0530

    Singleton and static  tasks completed

---
+++ com/KovanLearnings/OopArchitecturalThinking/ObjectCounter.java
@@ -0,0 +1,17 @@
+package com.KovanLearnings.OopArchitecturalThinking;
+
+public class ObjectCounter {
+    static int counter =0;
+    ObjectCounter(){
+        counter++;
+    }
+    public static void main(String []args){
+        new ObjectCounter();
+        System.out.println(ObjectCounter.counter);
+        new ObjectCounter();
+        System.out.println(ObjectCounter.counter);
+        new ObjectCounter();
+        System.out.println(ObjectCounter.counter);
+
+    }
+}
