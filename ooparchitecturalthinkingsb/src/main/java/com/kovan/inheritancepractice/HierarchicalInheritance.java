c83be066798c3da80eb1822a0b6aca0de3a9c615
Thaniyarasi <thaniyarasik@gmail.com>
2026-02-18 21:51:32 +0530

    Inhertitance practice problems

--- ooparchitecturalthinkingsb/src/main/java/com/kovan/inheritancepractice/HierarchicalInheritance.java
+++ ooparchitecturalthinkingsb/src/main/java/com/kovan/inheritancepractice/HierarchicalInheritance.java
@@ -1,4 +1,45 @@
 package com.kovan.inheritancepractice;
 
-public class HierarchicalInheritance {
+
+// Hierarchical - many child class extends same parent
+class Animal{
+    public void sound() {
+        System.out.println("Sound of the animal");
+    }
+}
+class Dog extends Animal{
+    Dog(){
+        System.out.println("Dog class");
+    }
+    public void sound() {
+        System.out.println("Dog Barks");
+    }
+}
+
+class Cat extends Animal{
+    Cat(){
+        System.out.println("Cat class");
+    }
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class HierarchicalInheritance extends Animal{
+    public void call(){
+        sound();
+    }
+    public static void main(String[] args) {
+        System.out.println("Animals");
+        HierarchicalInheritance hierarchicalInheritance = new HierarchicalInheritance();
+        hierarchicalInheritance.call();
+
+        Cat cat= new Cat();
+        Dog dog = new Dog();
+
+
+
+
+
+    }
 }
--- ooparchitecturalthinkingsb/src/main/java/com/kovan/inheritancepractice/MultipleInheritance.java
+++ ooparchitecturalthinkingsb/src/main/java/com/kovan/inheritancepractice/MultipleInheritance.java
@@ -1,4 +1,30 @@
 package com.kovan.inheritancepractice;
 
+interface Student{
+    void studentDetails(String name, int rollNo);
+}
+interface Professor{
+    void professorDetails(String name, int id );
+}
+
+class Department implements Student, Professor{
+    @Override
+    public void studentDetails(String name, int rollNo) {
+        System.out.println("Name: "+ name);
+        System.out.println("RollNo: "+rollNo);
+    }
+
+    @Override
+    public void professorDetails(String name, int id) {
+        System.out.println("Name: "+ name);
+        System.out.println("RollNo: "+id);
+    }
+}
+
 public class MultipleInheritance {
+    public static void main(String[] args) {
+        Department department= new Department();
+        department.professorDetails("Raja", 123);
+        department.studentDetails("Thani", 2265);
+    }
 }
